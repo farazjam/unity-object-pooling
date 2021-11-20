978d891946a3f26947857544e8cce9c72b031696
Faraz Jamshed <faraz.jamshed1@gmail.com>
2021-11-20 21:53:09 +0500

    Root

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] Sphere spherePrefab;
+    private Sphere[] spheres;
+    readonly int spawnCount = 10;
+    private int counter = 0;
+    [SerializeField] Button spawnButton;
+
+    void Start()
+    {
+        spheres = new Sphere[spawnCount];
+        Sphere sphere;
+        for (int i = 0; i < spawnCount; i++)
+        {
+            sphere = Instantiate(spherePrefab, this.transform);
+            spheres[i] = sphere;
+        }
+
+        spawnButton.onClick.AddListener(() => Spawn());
+    }
+
+    public void Spawn()
+    {
+        if (counter + 1 >= spawnCount) counter = 0;
+        spheres[counter++].Spawn(); // Fake Spawn (Already spawned)
+    }
+}
---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    private enum State { Pooled, Spawned };
+    private State state;
+    Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = transform.GetComponent<Rigidbody>();
+        Pool();
+    }
+
+    private void Pool()
+    {
+        state = State.Pooled;
+        this.rigidbody.isKinematic = true;
+        this.transform.localPosition = new Vector3((int)Random.Range(-10f, 10f), 0f, 0f);
+    }
+
+    public void Spawn()
+    {
+        if (state == State.Pooled)
+        {
+            state = State.Spawned;
+            this.rigidbody.isKinematic = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collector"))
+        {
+            Pool();
+        }
+    }
+}
